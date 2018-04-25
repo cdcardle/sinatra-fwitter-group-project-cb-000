de4aa3ad89db933d2753898b2abb5e0f9f5448a7
Chris Cardle <cdcardle@gmail.com>
2018-04-25 18:18:11 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   extend Slugifiable::ClassMethods
 
   has_many :tweets
+
+  def slug
+    self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|i| i.slug == slug}
+  end
 end
